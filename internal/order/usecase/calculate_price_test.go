3a103be9ce092b259fac0928269879182b1da000
Rony de Lucca <ronylucca@gmail.com>
2023-03-06 20:58:03 -0300

    tests update

--- internal/order/usecase/calculate_price_test.go
+++ internal/order/usecase/calculate_price_test.go
@@ -4,26 +4,51 @@ import (
 	"database/sql"
 	"testing"
 
-	_ "github.com/mattn/go-sqlite3"
+	"github.com/ronylucca/go-productfee-service/internal/order/entity"
+	"github.com/ronylucca/go-productfee-service/internal/order/infra/database"
 	"github.com/stretchr/testify/suite"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type CalculateFinalPriceUseCaseTestSuite struct {
+type CalculatePriceUseCaseTestSuite struct {
 	suite.Suite
-	Db *sql.DB
+	OrderRepository database.OrderRepository
+	Db              *sql.DB
 }
 
-func (suite *CalculateFinalPriceUseCaseTestSuite) SetupSuite() {
+func (suite *CalculatePriceUseCaseTestSuite) SetupSuite() {
 	db, err := sql.Open("sqlite3", ":memory:")
 	suite.NoError(err)
-	db.Exec("CREATE TABLE orders(id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
+	_, err = db.Exec("CREATE TABLE orders(id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
 	suite.Db = db
+	suite.OrderRepository = *database.NewOrderRepository(db)
 }
 
-func (suite *CalculateFinalPriceUseCaseTestSuite) TearDownTest() {
+func (suite *CalculatePriceUseCaseTestSuite) TearDownTest() {
 	suite.Db.Close()
 }
 
 func TestSuite(t *testing.T) {
-	suite.Run(t, new(CalculateFinalPriceUseCaseTestSuite))
+	suite.Run(t, new(CalculatePriceUseCaseTestSuite))
+}
+
+func (suite *CalculatePriceUseCaseTestSuite) TestCalculateFinalPrice() {
+	order, err := entity.NewOrder("1", 10, 2)
+	suite.NoError(err)
+	order.CalculateFinalPrice()
+
+	calculateFinalPriceInput := OrderInputDTO{
+		ID:    order.ID,
+		Price: order.Price,
+		Tax:   order.Tax,
+	}
+	calculateFinalPriceUseCase := NewCalculateFinalPriceUseCase(suite.OrderRepository)
+	output, err := calculateFinalPriceUseCase.Execute(calculateFinalPriceInput)
+	suite.NoError(err)
+	suite.Equal(order.ID, output.ID)
+	suite.Equal(order.Price, output.Price)
+	suite.Equal(order.Tax, output.Tax)
+	suite.Equal(order.FinalPrice, output.FinalPrice)
+
 }
